9feb5389282a1d0ce0d95bcd1aa64ab8548ab118
NormantasKudzma <kudzma.normantas@gmail.com>
2015-12-13 14:12:47 +0200

    Trying to make tests work #11

--- tests/api/GetPidCept.php
+++ tests/api/GetPidCept.php
@@ -2,7 +2,7 @@
 $I = new ApiTester($scenario);
 $I->wantTo('get new pid for a user');
 $I->haveHttpHeader('Content-Type', 'application/json');
-$I->sendPOST('action/GetPid.php', json_encode(array('action' => 'GetPid')));
+$I->sendPOST('/action/GetPid.php', json_encode(array('action' => 'GetPid')));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(["response" => "success"]);
