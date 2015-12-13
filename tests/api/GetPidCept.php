5602d984bb31872ab1b8c53e51766b38691596cb
NormantasKudzma <kudzma.normantas@gmail.com>
2015-12-13 12:06:18 +0200

    Added codeception

---
+++ tests/api/GetPidCept.php
@@ -0,0 +1,10 @@
+<?php 
+$I = new ApiTester($scenario);
+$I->wantTo('get new pid for a user');
+$I->haveHttpHeader('Content-Type', 'application/json');
+$I->sendPOST('action/GetPid.php', json_encode(array('action' => 'GetPid')));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(["response" => "success"]);
+$I->grabDataFromJsonResponse("pid");
+?>
