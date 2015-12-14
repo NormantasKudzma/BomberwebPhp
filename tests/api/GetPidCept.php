e8f34c785cc7d14b83a5d8c56bfafd1381d94f99
NormantasKudzma <kudzma.normantas@gmail.com>
2015-12-14 10:11:55 +0200

    Fixing tests #6

--- tests/api/GetPidCept.php
+++ tests/api/GetPidCept.php
@@ -1,10 +1,10 @@
-<?php 
-$I = new ApiTester($scenario);
-$I->wantTo('get new pid for a user');
-$I->haveHttpHeader('Content-Type', 'application/json');
-$I->sendPOST('/api/v1/action/GetPid.php', json_encode(array('action' => 'GetPid')));
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-$I->seeResponseContainsJson(["response" => "success"]);
-$I->grabDataFromJsonResponse("pid");
+<?php
+	$I = new ApiTester($scenario);
+	$I->wantTo('get new pid for a user');
+	$I->haveHttpHeader('Content-Type', 'application/json');
+	$I->sendPOST('/api/v1/action/GetPid.php', json_encode(array('action' => 'GetPid')));
+	$I->seeResponseCodeIs(200);
+	$I->seeResponseIsJson();
+	$I->seeResponseContainsJson(["response" => "success"]);
+	$I->grabDataFromJsonResponse("pid");
 ?>
