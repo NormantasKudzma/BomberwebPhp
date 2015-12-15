bd7392619562e0f8c1477c3cbab6bf10b66494b1
NormantasKudzma <kudzma.normantas@gmail.com>
2015-12-15 21:13:06 +0200

    Fixing #47

--- tests/api/GetPidCept.php
+++ tests/api/GetPidCept.php
@@ -1,7 +1,7 @@
 <?php
 	$I = new ApiTester($scenario);
 	$I->wantTo('get new pid for a user');
-	$I->setHeader("Content-Type", "application/json");
+	$I->haveHttpHeader("Content-Type", "application/json");
 	$I->sendPOST('http://localhost/api/v1/action/GetPid.php', json_encode(array('action' => 'GetPid')));
 	$I->seeResponseCodeIs(200);
 	$I->seeResponseIsJson();
